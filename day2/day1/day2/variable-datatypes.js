1816deb0a24bc1a2db5b7a0cf75bbe77d920d789
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-02-26 03:39:57 -0800

    javabasics

---
+++ day2/day1/day2/variable-datatypes.js
@@ -0,0 +1,32 @@
+
+/*
+Today am going to focus on variables and data types:
+numeric variables
+string variables
+boolean variables
+Array variables
+
+*/
+// 1.Numeric variables
+const age=18; // numeric variable
+const myName="ROSE";//string variables
+const isSheAGirl=true;//boolean variable
+const array=["string",12,false];
+const myFavFood =["beans","irish potato", "chips", "cassava meals","rice"];
+
+console.log("My name is " + myName + " and am " + age +  "years old");
+// console.log(`My name is ${myNme} and am ${age} years old`); new version
+console.log(array);
+console.log(myFavFood);
+
+/*
+variables declaration & variables definition
+
+*/
+
+var x;// this is a variables declaration but not a definition
+var y="shecancode";
+// variable declaration  & variable definition
+/* undestanding 
+
+ */
