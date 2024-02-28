336937fa11e1cdd959975ca6f3e82cd3f830fe63
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-02-28 02:33:34 -0800

    thisisforloops

---
+++ day3/arrynested.js
@@ -0,0 +1,21 @@
+const array=[ [1,2] , [3,4] , [6,7] ,9 , 10];
+console.log(array[4]);//the last array
+console.log(array[0][1]);// the index zero in value 2
+console.log(array[0][0]);// the index zero in value 1
+
+
+//for the index one in d nested array
+const arr=[["a","b"], ["c","d"] ,["e","f"] , "g","h"];
+console.log(arr[1][1]);
+
+//for obect
+let citizens={
+    "men":10,
+    "women":5,
+    
+}
+console.log(citizens.women)
+
+
+
+
---
+++ day3/forloops.js
@@ -0,0 +1,29 @@
+// FOR In used in obect
+const person= {
+    name : "rose",
+    age:"25",
+    sex:"female",
+
+
+};
+for(let key in person){
+console.log(key +':' + person[key])
+}
+
+
+
+
+// for each in array example one
+const numbers=[1,2,3,4,5,6,7,8,9]
+numbers.forEach(function(a){
+    console.log(" the numbers for" + a)
+});
+
+//example two in for each
+
+
+
+
+
+
+
