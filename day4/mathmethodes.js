2b76c0c7795d6f0dce5323254186ed590e1e8446
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-02-29 02:44:41 -0800

    thisisday4

---
+++ day4/mathmethodes.js
@@ -0,0 +1,52 @@
+const Num = -4;
+console.log(Math.abs(Num));
+
+const x = 2.5;
+console.log(Math.ceil(x));
+
+console.log(Math.random());
+console.log(Math.round(x));
+console.log(Math.floor(x));
+
+
+console.log(Math.round(x));
+
+
+//max and min methodes
+let a=[2,3,4,5,6];
+minNumber =Math.min(...a);
+maxNumber=Math.max(...a)
+console.log(minNumber)
+console.log(maxNumber)
+
+// math power
+let c=2;
+let d=4;
+console.log(Math.pow(c,d));
+
+// math srq
+let f=4;
+console.log(Math.sqrt(f))
+
+// sin and cos and tan
+let radians=180;
+console.log(Math.sin(radians))
+console.log(Math.cos(radians))
+console.log(Math.tan(radians))
+// log and exponetion
+
+let b=3;
+console.log(Math.log(3))
+ console.log(Math.log10(3))// exponetion
+
+
+
+
+
+
+
+
+
+
+
+
